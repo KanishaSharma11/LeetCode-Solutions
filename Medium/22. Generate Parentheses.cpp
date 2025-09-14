741b29fe82f8bf3a49d2dc37288f66eb08481271
Kanisha Ravindra Sharma <158239497+KanishaSharma11@users.noreply.github.com>
2025-09-14 12:35:40 +0530

    Create 22. Generate Parentheses.cpp

---
+++ Medium/22. Generate Parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        string current;
+        backtrack(n, 0, 0, current, ans);
+        return ans;
+    }
+    void backtrack(int n, int open, int close, string& current, vector<string>& ans){
+        if(current.size() == 2*n){
+            ans.push_back(current);
+            return;
+        }
+
+        if(open < n){
+            current.push_back('(');
+            backtrack(n, open + 1, close, current, ans);
+            current.pop_back();
+        }
+        if(close < open){
+            current.push_back(')');
+            backtrack(n, open, close + 1, current, ans);
+            current.pop_back();
+        }
+    }
+};
